fa7c00e697fd07e28ca81d9c5a384795a274df9a
Justin Salamon <justin.salamon@gmail.com>
2014-06-16 17:15:29 -0400

    added usage instructions and examples, changed default fs to 16000

--- melosynth.py
+++ melosynth.py
@@ -16,6 +16,62 @@ but can be used to synthesize any pitch sequence represented as a two-column txt
 or csv file where the first column contains timestamps and the second contains
 the corresponding frequency values in Hertz.
 
+@section USAGE
+
+usage: melosynth.py [-h] [--output OUTPUT] [--fs FS] [--nHarmonics NHARMONICS]
+                    [--square] [--useneg]
+                    inputfile
+
+positional arguments:
+  inputfile             Path to input file containing the pitch sequence
+
+optional arguments:
+  -h, --help            show this help message and exit
+  --output OUTPUT       Path to output wav file. If not specified a file will
+                        be created with the same path/name as inputfile but
+                        ending with ".wav".
+  --fs FS               Sampling frequency for the synthesized file. If not
+                        specified the default value of is 16000 Hz is used.
+  --nHarmonics NHARMONICS
+                        Number of harmonics (including the fundamental) to use
+                        in the synthesis (default is 1). As the number is
+                        increased the wave will become more sawtooth-like.
+  --square              Converge to square wave instead of sawtooth as the
+                        number of harmonics is increased.
+  --useneg              By default, negative frequency values (unvoiced
+                        frames) are synthesized as silence. Setting the useneg
+                        option will synthesize these frames using their
+                        absolute values (i.e. as voiced frames).
+
+
+@section EXAMPLES
+
+Basic usage, without any options:
+
+>python melosynth.py ~/Documents/daisy3_melodia.csv
+
+This Will create a file called daisy3_melodia.wav in the same folder as the
+input file (~/Documents/) and use all the default parameter values for the
+synthesis.
+
+Advanced usage, including all options:
+
+>python melosynth.py ~/Documents/daisy3_melodia.csv --output ~/Music/mynewfile.wav --fs 44100 --nHarmonics 10 --square --useneg
+
+Here we are providing a specified path for the output instead of the default
+location. Next we specify the sample rate for the output (44.1 kHz) instead of
+the default value of 16000 Hz. Next, we specify the number of harmonics to use
+(10) instead of the default value of 1. Normally, as the number of harmonics is
+increased the waveform will converge to a sawtooth wave, however, since we
+specify the --square option, it will converge to a square wave instead. Finally,
+by specifying the --useneg (use negative) option we make the script use the
+absolute value of the frequencies so that negative frequencies are not
+synthesized as silence (which is the default behaviour).
+
+@section INSTALLATION
+Simply download the script and run it from your terminal as instructed above.
+Dependencies: python (tested on 2.7) and numpy (http://www.numpy.org/)
+
 @section LICENSE
 
 MeloSynth: synthesize a melody
@@ -236,9 +292,9 @@ if __name__ == "__main__":
     parser.add_argument("--output", help="Path to output wav file. If \
                         not specified a file will be created with the same \
                         path/name as inputfile but ending with \".wav\".")
-    parser.add_argument("--fs", default=11025, help="Sampling frequency for the\
+    parser.add_argument("--fs", default=16000, help="Sampling frequency for the\
                         synthesized file. If not specified the default value \
-                        of is 11025 Hz is used.")
+                        of is 16000 Hz is used.")
     parser.add_argument("--nHarmonics", default=1, help="Number of harmonics \
                         (including the fundamental) to use in the synthesis \
                         (default is 1). As the number is increased the wave \
